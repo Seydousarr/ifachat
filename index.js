a978d74352d244fc1fcd676f50362aee80f82285
Elhadji Seydou Sarr <elseydousarr07@outlook.com>
2024-10-30 13:14:03 +0100

    mise a jour ifa chat

--- index.js
+++ index.js
@@ -2,55 +2,58 @@ const express = require("express");
 const http = require('http');
 const app = express();
 const server = http.Server(app);
-const io = require('socket.io')(server)
-//172.20.192.227
+const io = require('socket.io')(server);
 
 const ip = "127.0.0.1";
 const port = 4000;
 app.use(express.static('public'));
-app.get('/',(req,res)=>{
-   res.sendFile('index.html',{root:__dirname}); 
-})
-// un tableau pour suivre mes utilisateurs actifs
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+// Un tableau pour suivre les utilisateurs actifs
 const users = [];
 const publicMessages = [];
 
-//.....
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
+    socket.emit("init", { message: "Bienvenue cher client du chat" });
 
-    socket.emit("init",{message:"bienvenue cher client du chat"})
-
-    // attente de l'emit sendLog
-    socket.on("sendLog",(data)=>{
-        //console.log(socket.id);
-        // securisation par authenticator ...
+    // Attente de l'emit sendLog
+    socket.on("sendLog", (data) => {
         data.id = socket.id;
         users.push(data);
-        //console.dir(users);
-    })
-    socket.on("publicMessage",(data)=>{
+        
+        // Émettre la mise à jour de la liste des utilisateurs à tous les clients
+        io.emit("updateUserList", users);
+    });
+
+    socket.on("publicMessage", (data) => {
         data.id = socket.id;
         publicMessages.push(data);
-        //console.dir(publicMessages);
-        socket.broadcast.emit("publicMessageGlobal",data);
-    })
-    socket.on("disconnect",()=>{
-        //socket.id
-        let indexDisconnect;
-        users.forEach((element,index) => {
-            if(element.id === socket.id){
-                indexDisconnect = index;
-            }
-        });
-        // splice sert à supprimer une entrée de tableau à
-        // partir de son index (indexDisconnect)
-        users.splice(indexDisconnect,1);
-        console.dir(users);
-        
-    })
-})
+        socket.broadcast.emit("publicMessageGlobal", data);
+    });
 
-server.listen(port,ip,()=>{
-    console.log("Demarer sur http://"+ip+":"+port);
-    
-})
+    socket.on("privateMessage", (data) => {
+        const { recipientId, messageContent } = data;
+        const message = {
+            pseudo: data.pseudo,
+            messageContent,
+            date: new Date(),
+        };
+        // Émettre le message privé à l'utilisateur cible
+        socket.to(recipientId).emit("privateMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+        const indexDisconnect = users.findIndex(element => element.id === socket.id);
+        if (indexDisconnect !== -1) {
+            users.splice(indexDisconnect, 1);
+            // Émettre la mise à jour de la liste des utilisateurs à tous les clients
+            io.emit("updateUserList", users);
+        }
+    });
+});
+
+server.listen(port, ip, () => {
+    console.log("Démarré sur http://" + ip + ":" + port);
+});
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,51 +1,83 @@
-// declaration de variables
+// Code client (dans script.js)
+
+// déclaration de variables
 const sendPublic = document.querySelector("#sendPublic");
 const messagesPublic = document.getElementById("messagesPublic");
+const clientsList = document.getElementById("clientsList");
 const socket = io();
 const query = window.location.search;
 const urlParams = new URLSearchParams(query);
 const pseudo = urlParams.get("pseudo");
-console.log(pseudo);
 const pwd = urlParams.get("pwd");
-console.log(pwd);
-// declarations de fonction
-const displayMessage = (data)=>{
+
+// déclarations de fonction
+const displayMessage = (data) => {
     messagesPublic.innerHTML += `
     <div class="newMessage">
         <h2>${data.pseudo}</h2>
         <p class="content">${data.messageContent}</p>
         <p class="date">${data.date}</p>
-    </div>`
-}
+    </div>`;
+};
+
+// Fonction pour afficher la liste des utilisateurs connectés
+const displayUserList = (users) => {
+    clientsList.innerHTML = ''; // Réinitialiser la liste
+    users.forEach(user => {
+        clientsList.innerHTML += `
+            <p onclick="openPrivateChat('${user.id}', '${user.pseudo}')">${user.pseudo}</p>`;
+    });
+};
+
 tinymce.init({
     selector: '#textPublic',
     plugins: [
-        'advlist', 'autolink',
-        'lists', 'link', 'image', 'charmap', 'preview', 'anchor', 'searchreplace', 'visualblocks',
+        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap',
+        'preview', 'anchor', 'searchreplace', 'visualblocks',
         'fullscreen', 'insertdatetime', 'media', 'table', 'help', 'wordcount'
     ],
-    toolbar: 'undo redo | formatpainter casechange blocks | bold italic backcolor | ' +
-        'alignleft aligncenter alignright alignjustify | ' +
+    toolbar: 'undo redo | formatpainter casechange blocks | ' +
+        'bold italic backcolor | alignleft aligncenter alignright alignjustify | ' +
         'bullist numlist checklist outdent indent | removeformat | a11ycheck code table help'
 });
 
 socket.on("init", (data) => {
     console.dir(data);
-    socket.emit("sendLog", {
-        pseudo: pseudo,
-        pwd: pwd
-    });
-})
-sendPublic.addEventListener("click",()=>{
-    let messageContent = tinyMCE.get("textPublic").getContent();
-    let date = new Date();//UTC ou https://momentjs.com/
-    // envoie du message public au server
-    let data = {pseudo:pseudo,messageContent:messageContent,date:date}
-    socket.emit("publicMessage",data);
-    displayMessage(data);
+    socket.emit("sendLog", { pseudo: pseudo, pwd: pwd });
+});
 
-})
-socket.on("publicMessageGlobal",(data)=>{
+sendPublic.addEventListener("click", () => {
+    let messageContent = tinyMCE.get("textPublic").getContent();
+    let date = new Date();
+    let data = { pseudo: pseudo, messageContent: messageContent, date: date };
+    socket.emit("publicMessage", data);
+    displayMessage(data);
+});
+
+socket.on("publicMessageGlobal", (data) => {
     console.dir(data);
     displayMessage(data);
-})
+});
+
+// Écoute l'événement pour mettre à jour la liste des utilisateurs
+socket.on("updateUserList", (users) => {
+    displayUserList(users);
+});
+
+// Fonction pour ouvrir une discussion privée
+function openPrivateChat(recipientId, recipientPseudo) {
+    const privateMessageContent = prompt(`Envoyer un message privé à ${recipientPseudo}:`);
+    if (privateMessageContent) {
+        const messageData = {
+            recipientId: recipientId,
+            pseudo: pseudo,
+            messageContent: privateMessageContent
+        };
+        socket.emit("privateMessage", messageData);
+    }
+}
+
+// Écoute l'événement pour les messages privés
+socket.on("privateMessage", (data) => {
+    alert(`Message privé de ${data.pseudo}: ${data.messageContent}`);
+});
